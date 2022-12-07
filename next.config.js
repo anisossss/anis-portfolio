dbb01d2734fd52937c32cd78e274a8bae05633b6
anis khalef <anis.khalef@duetodata.com>
2022-12-07 11:38:56 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
 
-module.exports = withPWA({
-  webpack: (config) => {
-    config.module.rules.push({
-      // test: /\.mp3$/,
-      use: {
-        loader: "url-loader",
-      },
-    });
-    return config;
-  },
-  
+module.exports = withPWA({ 
   pwa: {
     dest: "public",
     register: true,
