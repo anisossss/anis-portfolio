faa513b6a24c151e9ee77e3038c4ff423030e23c
anis khalef <anis.khalef@duetodata.com>
2022-12-07 11:04:00 +0100

    first commit

---
+++ constants.js
@@ -0,0 +1,153 @@
+export const METADATA = {
+  title: "Portfolio | Anis Khalef",
+  description:
+    "Anis Khalef is a Full Stack Developer from Tunisia, interested in crafting beautiful and functional applications.",
+  siteUrl: "https://AnisKhalef.com/",
+  twitterHandle: "@Anis731",
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "home",
+  },
+  {
+    name: "Skills",
+    ref: "skills",
+  },
+  {
+    name: "Projects",
+    ref: "projects",
+  },
+  {
+    name: "Work",
+    ref: "work",
+  },
+  {
+    name: "Contact",
+    ref: "contact",
+  },
+];
+
+export const TYPED_STRINGS = [
+  "A pragmatic Full Stack Developer",
+  "I build things for the web",
+  "I create aesthetic and modern apps",
+  "I build Web3.0 dApps"
+];
+
+export const SOCIAL_LINKS = [
+  {
+    name: "mail",
+    url: "mailto: anisKhalef98@gmail.com",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/anis-khalef-11660a231/",
+  },
+  {
+    name: "github",
+    url: "https://github.com/anisossss",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/aniskhalef/",
+  },
+  {
+    name: "twitter",
+    url: "#",
+  },
+];
+
+export const SKILLS = {
+  languagesAndTools: [
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "sass",
+    "python",
+    "moralis",
+    "stripe",
+    "thirdweb"
+  ],
+  librariesAndFrameworks: [
+    "react",
+    "redux",
+    "nextjs",
+    "tailwindcss",
+    "styledcomponents",
+    "antdesign",
+    "chakra-ui",
+    "gsap",
+    "framer",
+    "spring",
+    "three"
+  ],
+  databases: [
+    "mysql",
+    "mongodb",
+    "firebase",
+  ],
+  other: ["nodejs", "postman", "nginx", "git", "gitlab"],
+  softwares: ["xd", "ps"],
+
+};
+
+export const PROJECTS = [
+  {
+    name: "Airbnb",
+    image: "/projects/airbnb.webp",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description: "Airbnb UI clone using NextJS + Tailwind CSS 🛏️",
+    gradient: ["#F14658", "#DC2537"],
+    url: "https://Anis73-airbnb.vercel.app/",
+    tech: ["react", "nextjs", "tailwindcss", "mapbox"],
+  },
+  {
+    name: "Medium",
+    image: "/projects/medium.webp",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "Medium UI clone using NextJS + Tailwind CSS ✍🏻",
+    gradient: ["#FFA62E", "#EA4D2C"],
+    url: "https://Anis73-medium.vercel.app/",
+    tech: ["typescript", "react", "nextjs", "tailwindcss", "sanity.io"],
+  },
+  {
+    name: "Inshorts",
+    image: "/projects/inshorts.webp",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description:
+    "Conversational Voice Controlled React News Application using Alan AI 🎙",
+    gradient: ["#000066", "#6699FF"],
+    url: "https://Anis73-inshorts.netlify.app/",
+    tech: ["react", "chakra-ui", "alan"],
+  },
+  {
+    name: "Tesla",
+    image: "/projects/tesla.webp",
+    blurImage: "/projects/blur/tesla-blur.webp",
+    description: "A Tesla React Native App 🏎️",
+    gradient: ["#142D46", "#2E4964"],
+    url: "https://github.com/Anis73/tesla",
+    tech: ["react"],
+  },
+];
+
+export const WORK = [
+  {
+    company: "DuetoData",
+    title: "Full Stack Web Developer ",
+    location: "Sousse, Tunisia",
+    range: "January 2022 - Now",
+    responsibilities: [
+      "Led the Full Stack revamp",
+      "Developed app integration with REST APIs, Google Maps, User Auth, and other libraries.",
+      "Implemented CRUD features for all the services and providers.",
+    ],
+    url: "https://duetodata.com/",
+  },
+
+];
+
+export const GTAG = "G-";
---
+++ next.config.js
@@ -0,0 +1,21 @@
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: "url-loader",
+      },
+    });
+    return config;
+  },
+  
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+  
+});
